e0e407404100e79c7e059d7654f7b2f4698d53d0
udit saurabh <uditsaurabh@udits-MacBook-Pro.local>
2024-06-02 12:51:47 +0530

    added helpers and github actions

---
+++ src/cloudinary-upload.ts
@@ -0,0 +1,56 @@
+import cloudinary, {
+  UploadApiErrorResponse,
+  UploadApiResponse,
+} from 'cloudinary';
+
+export function uploads(
+  file: string,
+  public_id?: string,
+  overwrite?: boolean,
+  invalidate?: boolean
+): Promise<UploadApiResponse | UploadApiErrorResponse | undefined> {
+  return new Promise((resolve, reject) => {
+    cloudinary.v2.uploader.upload(
+      file,
+      {
+        public_id: public_id,
+        overwrite: overwrite,
+        invalidate: invalidate,
+        resource_type: 'auto',
+      },
+      (
+        error: UploadApiErrorResponse | undefined,
+        result: UploadApiResponse | undefined
+      ) => {
+        if (error) resolve(error);
+        resolve(result);
+      }
+    );
+  });
+}
+export function videoUpload(
+  file: string,
+  public_id?: string,
+  overwrite?: boolean,
+  invalidate?: boolean
+): Promise<UploadApiResponse | UploadApiErrorResponse | undefined> {
+  return new Promise((resolve, reject) => {
+    cloudinary.v2.uploader.upload(
+      file,
+      {
+        public_id: public_id,
+        overwrite: overwrite,
+        invalidate: invalidate,
+        chunk_size: 50000,
+        resource_type: 'video',
+      },
+      (
+        error: UploadApiErrorResponse | undefined,
+        result: UploadApiResponse | undefined
+      ) => {
+        if (error) resolve(error);
+        resolve(result);
+      }
+    );
+  });
+}
